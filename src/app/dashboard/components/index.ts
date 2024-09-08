7d90aa33decd5c5c54fd6aeea661f8ad49fdfaa6
Rashmi Sharma <rashmisharmaofficial19019@gmail.com>
2024-09-08 17:06:17 +0530

    change in folder structure

--- src/app/dashboard/components/index.ts
+++
@@ -1,5 +0,0 @@
-import { CreateRecordComponent } from './create-record/create-record.component';
-
-export const components = [CreateRecordComponent];
-
-export * from './create-record/create-record.component';
--- src/app/dashboard/containers/index.ts
+++
@@ -1,7 +0,0 @@
-import { HomeComponent } from './home/home.component';
-import { TableComponent } from './table/table.component';
-
-export const containers = [HomeComponent, TableComponent];
-
-export * from './table/table.component';
-export * from './home/home.component';
